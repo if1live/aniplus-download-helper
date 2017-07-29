bd23852dd68752463114b921ba312393eadb44c1
if1live <libsora25@gmail.com>
2017-07-30 00:23:01 +0900

    promise 기반으로 변경

--- launcher.js
+++ launcher.js
@@ -34,29 +34,80 @@ function isDownloadSupported(doc) {
   return true;
 }
 
-app.get('/', function (req, res) {
-  //res.send('Hello World!');
-  var serial = req.query['serial'];
-  var url = "http://www.aniplustv.com/tv/vod_downList.asp?contentSerial=" + serial;
-  http.get(url, function (response) {
-    //console.log("Got response: " + res.statusCode);
-    var body = '';
-    response.on('data', function(chunk) {
-      body += chunk;
-    });
-    response.on('end', function() {
-      const dom = new JSDOM(body);
-      var doc = dom.window.document;
-      var ids = getVideoIds(doc);
-      var supported = isDownloadSupported(doc);
-      res.render('launcher', {
-        serial: serial,
-        ids: ids,
-        supported: supported,
+function getProgramInfo(doc) {
+  var title = '';
+  var titleElem = doc.querySelector('.tit');
+  if(titleElem.innerText != '') {
+    title = titleElem.innerText;
+  } else {
+    title = titleElem.children[0].getAttribute('alt');
+  }
+
+  var subtitle = doc.querySelector('.stit').innerText;
+  return {
+    'title': title,
+    'subtitle': subtitle
+  };
+}
+
+function createProgramInfoPromise(serial) {
+  return new Promise(function(resolve, reject) {
+    var url = 'http://www.aniplustv.com/tv/program_view.asp?contentSerial=' + serial;
+    http.get(url, function (response) {
+      //console.log("Got response: " + res.statusCode);
+      var body = '';
+      response.on('data', function(chunk) {
+        body += chunk;
       });
+      response.on('end', function() {
+        const dom = new JSDOM(body);
+        var doc = dom.window.document;
+        var info = getProgramInfo(doc);
+        resolve(info);
+      });
+    }).on('error', function (e) {
+      reject(e);
     });
-  }).on('error', function (e) {
-    res.send("Got error: " + e.message);
+  });
+}
+
+function createVideoInfoPromise(serial) {
+  return new Promise(function(resolve, reject) {
+    var url = "http://www.aniplustv.com/tv/vod_downList.asp?contentSerial=" + serial;
+    http.get(url, function (response) {
+      //console.log("Got response: " + res.statusCode);
+      var body = '';
+      response.on('data', function(chunk) {
+        body += chunk;
+      });
+      response.on('end', function() {
+        const dom = new JSDOM(body);
+        var doc = dom.window.document;
+        var ids = getVideoIds(doc);
+        var supported = isDownloadSupported(doc);
+        var data = {
+          serial: serial,
+          ids: ids,
+          supported: supported,
+        };
+        resolve(data);
+      });
+    }).on('error', function (e) {
+      reject(e);
+    });
+  });
+}
+
+app.get('/', function (req, res) {
+  var serial = req.query['serial'];
+  var p1 = createProgramInfoPromise(serial);
+  var p2 = createVideoInfoPromise(serial);
+  Promise.all([p1, p2]).then(function(values) {
+    var data = {};
+    for(var i = 0 ; i < values.length ; i++) {
+      data = Object.assign(data, values[i]);
+    }
+    res.render('launcher', data);
   });
 });
 
