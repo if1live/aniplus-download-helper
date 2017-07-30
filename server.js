ecfb66b1fdba593b78221d8238733ca85d5ae7fc
if1live <libsora25@gmail.com>
2017-07-30 13:22:23 +0900

    애니플러스는 마지막 하나를 버린다. 그래서 , 로 끝나야한다

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   var serial = req.query['serial'];
   var p = detail.createPromise(serial);
   p.then(function (data) {
+    var ids_line = '';
+    for(var i = 0 ; i < data.ids.length ; i++) {
+      ids_line += data.ids[i];
+      ids_line += ',';
+    }
+    data['ids_line'] = ids_line;
     res.render('launcher', data);
   });
 });
